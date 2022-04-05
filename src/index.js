1a29d9bd256a2c0c22fe642ef962aca5436eb4d5
glaudepadua <glaudepadua@gmail.com>
2022-04-04 23:24:13 -0300

    Iniciando o ambiente: CRA e Material UI

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from 'theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* theme={theme} => {...{ theme }}
+    quando repete o nome da propriedade, abre o objeto passa um square operator e fica ->  {...{ theme }} */}
+    <ThemeProvider {...{ theme }}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
