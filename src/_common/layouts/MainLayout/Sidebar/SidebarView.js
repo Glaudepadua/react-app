83ef35f6f311634736ebbbe90f3351dc23a26dd5
glaudepadua <glaudepadua@gmail.com>
2022-04-25 22:44:12 -0300

    Configuration of MenuItem and it's view

--- src/_common/layouts/MainLayout/Sidebar/SidebarView.js
+++ src/_common/layouts/MainLayout/Sidebar/SidebarView.js
@@ -1,6 +1,46 @@
 import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import CheckCircleOutlineOutlined from "@material-ui/icons/CheckCircleOutlineOutlined";
+import DashboardOutlinedIcon from "@material-ui/icons/DashboardOutlined";
+import PeopleAltOutlinedIcon from "@material-ui/icons/PeopleAltOutlined";
 import clsx from "clsx";
+import MenuItem from "./MenuItem";
 import useStyles from "./SidebarStyle";
+
+const menu = [
+  {
+    text: "Dashboard",
+    icon: <DashboardOutlinedIcon />,
+    path: "/dashboard"
+  },
+  {
+    text: "Users",
+    icon: <PeopleAltOutlinedIcon />,
+    path: "/users"
+  },
+  {
+    text: "Tasks",
+    icon: <CheckCircleOutlineOutlined />,
+    items: [
+      {
+        text: "Mine",
+        path: "/tasks/mine"
+      },
+      {
+        text: "Opened",
+        path: "/tasks/opened"
+      },
+      {
+        text: "Finished",
+        path: "/tasks/finished"
+      },
+      {
+        text: "All",
+        path: "/tasks"
+      }
+    ]
+  }
+];
 
 const SidebarView = ({ sidebarOpen }) => {
   const classes = useStyles();
@@ -14,7 +54,11 @@ const SidebarView = ({ sidebarOpen }) => {
           [classes.drawerClose]: !sidebarOpen
         })
       }}>
-      <>Menu</>
+      <List>
+        {menu.map((item, index) => (
+          <MenuItem key={index} {...{ item }} />
+        ))}
+      </List>
     </Drawer>
   );
 };
